df52aa1a42768aa7781662b93ec2ec8e5537471b
thais <trevillon@jehann.fr>
2023-01-12 16:11:02 +0100

    :sparkles: requêtes

---
+++ exemple/requests.js
@@ -0,0 +1,97 @@
+import {log} from "util";
+
+function request() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(120)
+           // reject({message: "I'm a teapot !", status: 418})
+
+        }, 3000)
+    })
+} // youhouh
+
+function getById(id) {
+    return new Promise((resolve, reject) => {
+        resolve({id, text: 'Youhouh !'})
+    })
+}
+
+
+
+let id;
+let article;
+request()
+    .then((response) => {
+        id = response;
+        console.log(id); // youhouh
+        return getById(id) // retourne une promesse
+    })
+    .then((response) => article = response)
+    .catch((error) => console.log(`[ERROR ${error.status}] : ${error.message}`))  // catch toutes les erreurs
+    .finally(() => console.log('Finito !'))
+
+console.log(id); // undefined
+
+
+
+let text;
+request()
+    .then((id) => getById(id))
+    .then(res => text = res)
+    .catch(err => console.log(err.message))
+
+
+// Async Await
+
+async function getDatas() {
+    // On peut faire des opérations asynchrones, manipuler des promesses
+    // mais l'intérieur de la fonction est "synchrone"
+    // On met "await" devant chaque promesse / fonction asynchrone pour attendre sa résolution avant de passer à la suite
+    try {
+        const id = await request(); // attend la résolution de la promesse pour enregistrer le résultat
+        console.log(id);
+        const article = await getById(id);
+        console.log(article);
+        //
+    } catch(e) {
+        console.log(e.message || e);
+    } finally {
+        console.log('finito')
+    }
+}
+getDatas(); // asynchrone
+
+
+fetch('www.monapi.fr/user')
+    .then(res => res.json()) // ligne obligatoire pour accéder aux données
+    .then(data => console.log(data))
+    .catch(e => console.log(e))
+
+async function getUsers() {
+    try {
+        const res = await fetch('url/chemin');
+        const datas = await res.json();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+/**********
+ * SOIT -
+ *      HTML + Script / fichier JS -> console du navigateur
+ *      Un fichier JS +   node chemin/monfichier.js    => console de l'IDE
+ *
+ *  https://jsonplaceholder.typicode.com/users
+ *  Utiliser JSON.placehoder pour afficher dans la console la liste des utilisateurs
+ *  Utiliser JSON placeholder pour afficher l'utilisateur ayant l'id 1
+ *
+ *  Créer une fonction qui permet d'afficher un utilisateur via son id
+ *  ( appeler cette fonction avec les ID 1, 5, 20 )
+ *  => Si il n'y a pas d'utilisateurs, ou en cas d'erreur afficher "Aucune utilisateur⋅trice trouvé⋅e
+ *
+ *  Faire une fonction pour récupérer un post ( /posts ) par son ID
+ *  + Récupérer l'utilisateur qui a créé le post
+ *  => Afficher dans la console "[ Nom de l'utilisateur⋅trice ] : titre du post"
+ *
+ */
